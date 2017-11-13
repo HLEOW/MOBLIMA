b4e6ca773b505f61bff276841beaa253e1ee0c9d
HLEOW <26955163+HLEOW@users.noreply.github.com>
2017-11-13 13:12:30 +0800

    Update StaffManager.java

--- src/com/moblima/Manager/StaffManager.java
+++ src/com/moblima/Manager/StaffManager.java
@@ -1,5 +1,15 @@
 package com.moblima.Manager;
+import com.moblima.users.Staff;
+import com.moblima.movie.*;
+
+import java.util.Date;
 
 public class StaffManager{
+    private Staff staff;
+    public StaffManager(Staff staff){
+        this.staff= staff;
+    }
+    public string configureSystem( MovieListing movieListing, ShowTime showTime, Cinema cinema){
+    }
     
 }
